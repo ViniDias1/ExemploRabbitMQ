57e82ec4344dbb0e8899b05f6fdbd4992b6c4535
Ubuntu <ubuntu@ip-172-31-29-203.ec2.internal>
2023-12-09 00:55:20 +0000

    ExemploRabbitMQ

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
@@ -10,9 +10,9 @@ public class Emissor {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
+    factory.setHost("ec2-54-198-173-46.compute-1.amazonaws.com"); // Alterar
+    factory.setUsername("admin"); // Alterar
+    factory.setPassword("password"); // Alterar
     factory.setVirtualHost("/");    Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
